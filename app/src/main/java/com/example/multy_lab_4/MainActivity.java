f2133189b605b4b98165f9e4570fb6bd0502f1bd
satoshi <uglysatoshi@ya.ru>
2024-02-27 05:00:38 +0300

    complete

---
+++ app/src/main/java/com/example/multy_lab_4/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.multy_lab_4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button b = findViewById(R.id.button);
+        b.setOnClickListener(view -> {
+            finish();
+        });
+    }
+}
